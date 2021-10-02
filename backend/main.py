2ea57289f33cd088dfbd549cd1a2ee29fd0fc052
Rob Reutiman <rob.reutiman@parivedasolutions.com>
2021-10-02 12:31:53 -0700

    Initial Backend setup

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,15 @@
-from typing import Optional
+import os
 
 from fastapi import FastAPI
+from Mangum import Mangum
 
-app = FastAPI()
+stage = os.environ.get('STAGE', None)
+openapi_prefix = f"/stage" if stage else "/
+"
+app = FastAPI(title="LanternNetworkBackend", openapi_prefix=openapi_prefix)
+
+@app.get("/hello")
+def hello_world():
+    return {"message": "Hello World!"}
+
+handler = Mangum(app)
