5aad14cc18c64c8e6a06b8f7ddc46581cc0f1afd
Rob Reutiman <rob.reutiman@parivedasolutions.com>
2021-10-02 23:42:49 -0700

    Profile page

--- lantern-network/src/App.js
+++ lantern-network/src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import {Box} from '@material-ui/core';
 import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import {LogIn, SignUp, Profile} from './pages/Pages';
+import Footer from './footer';
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/signup' render={(props) => <SignUp {...props}/>}/>
-        <Route path='/' render={(props) => <LogIn {...props}/>}/>
         <Route path='/me' render={(props) => <Profile {...props}/>}/>
+        <Route path='/' render={(props) => <LogIn {...props}/>}/>
       </Switch>
+      <Footer/>
     </BrowserRouter>
   );
 }
--- lantern-network/src/pages/Profile.js
+++ lantern-network/src/pages/Profile.js
@@ -1,26 +1,106 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function Profile(props) {
+import "./Profile.css";
+import defaultImage from '../assets/profile_image.svg';
+import axios from "axios";
 
-  const mockStudent = {
-    name: "Me",
+const Profile = (props) => {
+
+  const mockProfile = {
+    fullName: "Me",
     pronouns: "they/them",
+    type: "mentee",
     school: "University of College",
-    interests: "hobbies",
+    email: "email@website.com",
+    interests: ["hobbies", "activities", "fun", "things", "stuff"],
+    image: defaultImage
   }
 
-  const [name, setName] = useState("Me");
-  
+  const mockPartner = {
+    fullName: "You",
+    pronouns: "they/them",
+    type: "mentor",
+    occupation: "job",
+    email: "email@website.com",
+    experience: ["STEM", "Law"],
+    image: defaultImage
+  }
+
+  const [profile, setProfile] = useState(mockProfile);
+  const [partner, setPartner] = useState(mockPartner);
+  const [editing, toggleEditing] = useState(false);
+
+
+  useEffect(() => {
+
+    async function fetchData() {
+
+      var response = await axios.get("https://q61b1ddpui.execute-api.us-east-2.amazonaws.com/beta/mentor/test-id-8");
+      console.log(response);
+      // setProfile(response);
+      // TODO: Fetch image from s3
+      // response = axios.get()
+      // setPartner(response);
+      }
+
+    fetchData();
+  });
+
+  // TODO: Imeplement API call to upload image to s3
+  const uploadNewImage = (img) => {
+
+  }
 
   return (
     <div className="content-container">
-      <div className="information-container">
-        <div className="my-info">
-          <h1>{name}</h1>
+      {/* Profile Info */}
+      <div className="info-container">
+        <div className="info">
+          <div>
+            <h1>{profile.fullName}</h1>
+            <p>({profile.pronouns})</p>
+          </div>
+          <div className="break">
+            {profile.type === "mentee" ? (<p>School: {profile.school}</p>) : (<p>Occupation: {profile.occupation}</p>)}
+            <p>Contact: {profile.email}</p>
+            {profile.type === "mentee" ? (<p>Interests: {profile.interests.join(", ")}</p>) : <p>Experience: {profile.experience.join(", ")}</p>}
+          </div>
+            {profile.type === "mentor" ? (
+            <div className="break">
+              <p>Background Check: {profile.passedBackgroundCheck ? "Complete" : "Incomplete"}</p>
+              <p>Training: {profile.trainingComplete ? "Complete" : "Incomplete"}</p>
+            </div>
+            ) : null}
+        </div>
+        <div className="info">
+          <img src={profile.image} alt="<profile_photo>"/>
         </div>
       </div>
-    </div>  
-  )
+      {/* <div className="edit-button-container">
+        <button className="edit-profile">Edit Profile</button>
+      </div> */}
+
+
+      {/* Partner Info */}
+      {/* TODO: Only render if partner exists */}
+      <div className="info-container">
+        <div className="info">
+          <div>
+            <h2>{partner.type === "mentor" ? "Mentor": "Mentee"}:</h2>
+            <h3>{partner.fullName}</h3>
+            <p>({partner.pronouns})</p>
+          </div>
+          <div className="break">
+            {partner.type === "mentor" ? (<p>Occupation: {partner.occupation}</p>): (<p>School: {partner.school}</p>)}
+            <p>Contact: {partner.email}</p>
+          </div>
+        </div>
+        <div className="info">
+          <img className="partner-photo" src={partner.image} alt="<profile_photo>"/>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
