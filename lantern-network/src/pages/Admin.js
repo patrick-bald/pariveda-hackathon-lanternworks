194c99a0103232afb06f891d6d65cd5e0c27d516
Cayla Seaman <cayla.seaman@parivedasolutions.com>
2021-10-02 22:07:53 -0700

    admin table

--- lantern-network/src/pages/Admin.js
+++ lantern-network/src/pages/Admin.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 // import EmployeeForm from "./EmployeeForm";
 // import PageHeader from "../../components/PageHeader";
 // import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone';
-import { Paper, makeStyles, TableBody, TableRow, TableCell, Toolbar, InputAdornment } from '@material-ui/core';
+import { Paper, makeStyles, TableBody, TableRow, TableCell, Collapse, Box, Typography, Table, TableHead } from '@material-ui/core';
 import useTable from "../components/useTable";
 // import * as employeeService from "../../services/employeeService";
 // import Controls from "../../components/controls/Controls";
@@ -21,6 +21,7 @@ const useStyles = makeStyles(theme => ({
 
 const headCells = [
     { id: 'fullName', label: 'Mentor' },
+    { id: 'mentees', label: 'Mentees' },
     { id: 'email', label: 'Email' },
     { id: 'occupation', label: 'Occupation' },
     { id: 'pronouns', label: 'Pronouns' },
@@ -30,52 +31,57 @@ const headCells = [
 
 const payload = [
     {
-        "fullName": "Cayla S", 
-        "email": "@", 
+        "fullName": "Cayla S",
+        "email": "@",
         "occupation": "Business",
-        "pronouns":"she/her",
-        "backgroundCheck":"",
-        "training":"",
+        "pronouns": "she/her",
+        "backgroundCheck": "",
+        "training": "",
 
-       
+
     },
     {
-        "fullName": "Rob R", 
-        "email": "@", 
+        "fullName": "Rob R",
+        "email": "@",
         "occupation": "Business",
-        "pronouns":"he/him",
-        "backgroundCheck":"Done",
-        "training":"Done",
-       
+        "pronouns": "he/him",
+        "backgroundCheck": "Done",
+        "training": "Done",
+
     },
     {
-        "fullName": "Patrick B", 
-        "email": "@", 
+        "fullName": "Patrick B",
+        "email": "@",
         "occupation": "Tech",
-        "pronouns":"he/him",
-        "backgroundCheck":"",
-        "training":"Done",
-       
+        "pronouns": "he/him",
+        "backgroundCheck": "",
+        "training": "Done",
+
     },
     {
-        "fullName": "Chris P", 
-        "email": "@", 
+        "fullName": "Chris P",
+        "email": "@",
         "occupation": "Business",
-        "pronouns":"he/him",
-        "backgroundCheck":"Done",
-        "training":"",
-       
+        "pronouns": "he/him",
+        "backgroundCheck": "Done",
+        "training": "",
+
     }
 ]
-
-export default function Employees() {
+const mentees = [
+    {
+        "name" : "test",
+        "pronoun" : "they/them"
+    }
+]
+export default function Admin() {
 
 
 
     const classes = useStyles();
     const [records, setRecords] = useState(payload);
     const [filterFn, setFilterFn] = useState({ fn: items => { return items; } });
-
+    const [open, setOpen] = React.useState(false);
     const {
         TblContainer,
         TblHead,
@@ -117,16 +123,50 @@ export default function Employees() {
                     <TableBody>
                         {
                             recordsAfterPagingAndSorting().map(item =>
-                                (<TableRow key={item.id}>
-                                    <TableCell>{item.fullName}</TableCell>
-                                    <TableCell>{item.email}</TableCell>
-                                    <TableCell>{item.occupation}</TableCell>
-                                    <TableCell>{item.pronouns}</TableCell>
-                                    <TableCell>{item.backgroundCheck}</TableCell>
-                                    <TableCell>{item.training}</TableCell>
-                                </TableRow>)
+                            (<TableRow key={item.id}>
+                                <TableCell>{item.fullName}</TableCell>
+                                <TableCell onClick={() => setOpen(!open)}>Click</TableCell>
+                                <TableCell>{item.email}</TableCell>
+                                <TableCell>{item.occupation}</TableCell>
+                                <TableCell>{item.pronouns}</TableCell>
+                                <TableCell>{item.backgroundCheck}</TableCell>
+                                <TableCell>{item.training}</TableCell>
+                            </TableRow>
+
+                            )
                             )
                         }
+                        <TableRow>
+                            <Collapse in={open} timeout="auto" unmountOnExit>
+                                <Box sx={{ margin: 1 }}>
+                                    <Typography variant="h6" gutterBottom component="div">
+                                        History
+                                    </Typography>
+                                    <Table size="small" >
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>Mentee</TableCell>
+                                                <TableCell>Pronoun</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+
+                                            {
+                                                mentees.map(item =>
+                                                (<TableRow key={item.id}>
+                                                    <TableCell>{item.name}</TableCell>
+                                                    <TableCell>{item.pronoun}</TableCell>
+                                                </TableRow>
+
+                                                )
+                                                )
+                                            }
+
+                                        </TableBody>
+                                    </Table>
+                                </Box>
+                            </Collapse>
+                        </TableRow>
                     </TableBody>
                 </TblContainer>
                 <TblPagination />
